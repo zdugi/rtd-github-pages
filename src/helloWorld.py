ea4af41f0a2285055899158e0279e0d11493478a
Zdravko Dugonjic <zdravko.dugi@gmail.com>
2022-05-22 12:39:44 +0200

    Update helloWorld.py

--- src/helloWorld.py
+++ src/helloWorld.py
@@ -2,6 +2,7 @@
 def helloWorld( msg ):
     """
     This is a simple function that prints a message
+    And new author made a little change.
  
     Parameters:
        msg (str): The string to print
